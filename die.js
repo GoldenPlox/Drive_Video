a814bd6ffb459f712a62a44a3fc1c6e0a2f080ab
GoldenPlox <115761630+GoldenPlox@users.noreply.github.com>
2023-02-18 17:57:43 -0500

    first commint

--- die.js
+++ die.js
@@ -1,28 +1,31 @@
-let url= prompt("Ingrese la url")
+console.log("Start")
 
+let url= prompt("Ingrese la url");
+//https://drive.google.com/file/d/1MS0syTU55lXwEmiTl_uH5jZDglq5FmbM/view?usp=share_link
 let res="";
 let posIni=0;
 let posFin=0;
 
-for( let i=0; i<url.lenght; i++){
+for(let i=0;i<url.length;i++){
 
-    if( url.substring(i,i+2) === "d/"){
+    if( url.substring(i,i+2)=== "d/"){
         posIni=i+2;
     }
 
     if(url.substring(i,i+2)=== "/v"){
-        posFin=i;
-            
+        posFin=i;   
+
     }
 }
 
 res = url.substring(posIni, posFin);
-console.log("Resultado del string:"+ res);
+console.log("Resultado del string: "+ res);
 
-let doc = document.createElement("img01");
-let cadena='https://drive.google.com/file/d/1MS0syTU55lXwEmiTl_uH5jZDglq5FmbM/view?usp=share_link'+res;
+let doc = document.createElement("img");
+let cadena = 'https://drive.google.com/uc?export=view&id=' + res;
+//doc.src='https://drive,google.com/uc?export=viem&id=${res}';
+console.log("resultado del string:" +res+ "url completa:"+ cadena);
 
-console.log("resultad de string:" +res+ "url completa:"+ cadena);
-
-doc.src= cadena;
-let d= document.getElementById("ruta");
+doc.src=cadena;
+let d = document.getElementById("ruta");
+d.appendChild(doc);
